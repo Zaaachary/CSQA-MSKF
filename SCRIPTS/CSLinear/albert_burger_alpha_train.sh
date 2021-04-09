5b4f8b24e481f146491dbfabe827f68f4fb0ddde
Zaaachary <li_zaaachary@163.com>
2021-04-09 18:30:35 +0800

    update sh

--- SCRIPTS/CSLinear/albert_burger_alpha_train.sh
+++ SCRIPTS/CSLinear/albert_burger_alpha_train.sh
@@ -1,25 +1,24 @@
 python CODE/run_task.py\
-    --task_name CSLinear_Albert_BurgerAlpha2\
+    --task_name CSLinear_Albert_BurgerAlpha3\
     --mission train\
     --fp16 0\
-    --gpu_ids 0\
+    --gpu_ids 1\
     --save_mode step\
-    --print_step 150\
+    --print_step 100\
     --eval_after_tacc 0.59\
     --evltest_batch_size 12\
-    --clip_batch_off\
     \
-    --cs_num 4\
+    --cs_num 3\
     --max_qa_len 54\
     --max_cs_len 20\
     --max_seq_len 140\
-    --OMCS_version 2\
+    --OMCS_version 1\
+    --albert1_layers 10\
     \
     --train_batch_size 8\
-    --albert1_layers 10\
     --gradient_accumulation_steps 1\
     --learning_rate 2e-5\
-    --num_train_epochs 2\
+    --num_train_epochs 8\
     --warmup_proportion 0.1\
     --weight_decay 0.1\
     \
--- SCRIPTS/OMCS/omcs2_albert_train.sh
+++ SCRIPTS/OMCS/omcs2_albert_train.sh
@@ -2,24 +2,24 @@ python CODE/run_task.py\
     --task_name OMCS_Albert_Baseline\
     --mission train\
     --fp16 0\
-    --gpu_ids 0\
+    --gpu_ids 1\
     --save_mode step\
     --seed 42\
-    --print_step 500\
+    --print_step 100\
     --evltest_batch_size 12\
-    --eval_after_tacc 0.60\
+    --eval_after_tacc 0.58\
     \
-    --cs_num 1\
+    --cs_num 3\
     --max_seq_len 140\
-    --OMCS_version 2\
+    --OMCS_version 2.2\
     \
-    --train_batch_size 1\
-    --gradient_accumulation_steps 8\
+    --train_batch_size 8\
+    --gradient_accumulation_steps 1\
     --learning_rate 2e-5\
-    --num_train_epochs 10\
+    --num_train_epochs 8\
     --warmup_proportion 0.1\
     --weight_decay 0.1\
     \
     --dataset_dir DATA\
     --result_dir  /data/zhifli/model_save\
-    --PTM_model_vocab_dir /home/zhifli/DATA/transformers-models/albert-xxlarge-v2\
+    --PTM_model_vocab_dir /home/zhifli/DATA/transformers-models/albert-base-v2\
