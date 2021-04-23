a0008acb2771d0e80d0691467fb73c54f0e0c956
Zachary-LAB02 <li.zaaachary@163.com>
2021-04-23 13:45:11 +0800

    update ptm

--- CODE/ptm_task/data.py
+++ CODE/ptm_task/data.py
@@ -16,3 +16,14 @@ from torch.utils.data import DataLoader, RandomSampler, TensorDataset
 from ptm_task.example import *
 
 
+class ProcessorBase(object):
+
+    def __init__(self, args, dataset_type) -> None:
+        self.args = args
+        self.dataset_dir = args.dataset_dir
+        self.dataset_type = dataset_type
+
+    def load_data(self):
+        pass
+
+    
