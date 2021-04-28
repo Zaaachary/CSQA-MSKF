55654be2cb2aa6b06babd5b6e8e87bfd2047aae0
Zachary-LAB02 <li.zaaachary@163.com>
2021-04-28 08:22:13 +0800

    update predict

--- CODE/utils/predict_format.py
+++ CODE/utils/predict_format.py
@@ -20,11 +20,11 @@ import pandas
 raw_prediction_dir = r"D:\CODE\Commonsense\CSQA_dev\DATA\result\prediction"
 
 origin_dir = raw_prediction_dir + r"\predict.json"
+# origin_dir = raw_prediction_dir + r"\predict.json"
 # origin_dir = r"D:\CODE\Commonsense\CSQA_dev\DATA\result\albert-base-v2\WKDT_Albert_Baseline\2238-Apr19_seed42_wkdtv3.0\predict.json"
 output_dir = raw_prediction_dir + r"\questions.json"
 
 output_dir2 = raw_prediction_dir + r"\predictions.csv"
-# output_dir = r"D:\CODE\Commonsense\CSQA_dev\DATA\result\albert-base-v2\WKDT_Albert_Baseline\2238-Apr19_seed42_wkdtv3.0\predict_format.json"
 
 def load_result():
     f = open(origin_dir, 'r', encoding='utf-8')
@@ -48,16 +48,16 @@ def create_jsonl(all_case):
         f.write(s+'\n')
     f.close()
 
-# def create_csv(all_case):
-#     f = open(output_dir2, 'w', encoding='utf-8')
-#     for case in all_case:
-#         s = f"{case['id'],case['answer_Key']}"
-#         f.write(s+'\n')
-#     f.close()
+def create_csv(all_case):
+    f = open(output_dir2, 'w', encoding='utf-8')
+    for case in all_case:
+        s = f"{case['id']}, {case['answerKey']}"
+        f.write(s+'\n')
+    f.close()
 
 
 if __name__ == "__main__":
     all_case = load_result()
-    create_jsonl(all_case)
-    # create_csv(all_case)
+    # create_jsonl(all_case)
+    create_csv(all_case)
 
